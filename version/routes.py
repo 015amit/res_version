fdea6aae26dd63aaae54580cbdb42c11d835a0bf
019anshu <anshu.lakra25@gmail.com>
2021-06-07 13:31:31 +0530

    updated

--- version/routes.py
+++ version/routes.py
@@ -78,6 +78,10 @@ def teams(name):
 def page_not_found(e):
     return render_template('404.html', title="Page Not Found"), 404
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html', title="Internal Server Error"), 500
+
 
 @app.route('/show/<int:id>')
 def getimg(id):
