7801df2d6e4e83d9669762f552aeb14cf84807d3
015amit <ak2383977@gmail.com>
2021-06-12 20:48:59 +0530

    updated now

--- version/events.py
+++ version/events.py
@@ -2,8 +2,8 @@ allevents = {
     "event": [
         {"id": 1 ,"name": "Scrim 1","desc": "This is a coding based event",
          "rules": "Solution should not be made public during the events"},
-        {"id": 2,"name": "Scrim 2","desc": "You got a chance to show your css skills",
-        "rules": "Copying is not allowed here"},
+        # {"id": 2,"name": "Scrim 2","desc": "You got a chance to show your css skills",
+        # "rules": "Copying is not allowed here"},
         # {"id": 3,"name": "Cicada","desc": "Solve basic questions but in less time"},
         # {"id": 4,"name": "Mothra","desc": "Guess the logic and apply to get the answers"}
     ]
