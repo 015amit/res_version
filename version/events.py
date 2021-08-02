29ec690bbb701c2bb558663c0f1bd219ef2c31c1
015amit <ak2383977@gmail.com>
2021-08-03 01:48:54 +0530

    workshop

--- version/events.py
+++ version/events.py
@@ -4,7 +4,7 @@ allevents = {
          "rules": "Solution should not be made public during the events"},
         {"id": 2,"name": "Scrim 2","desc": "You got a chance to show your css skills",
         "rules": "Copying is not allowed here"},
-        {"id": 3,"name": "Workshop","desc": "You got a chance to show your css skills",
+        {"id": 3,"name": "Workshop on D-Apps","desc": "You got a chance to show your css skills",
         "rules": "Copying is not allowed here"},
         # {"id": 3,"name": "Cicada","desc": "Solve basic questions but in less time"},
         # {"id": 4,"name": "Mothra","desc": "Guess the logic and apply to get the answers"}
