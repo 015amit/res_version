9d1f431a2945613462b7f6c3342ab810e31325ab
015amit <ak2383977@gmail.com>
2021-10-20 23:22:02 +0530

    final06

--- version/events.py
+++ version/events.py
@@ -3,8 +3,7 @@ allevents = {
         
         {"id": 5,"name": "Codathon","desc": "You got a chance to show your css skills",
         "rules": "Copying is not allowed here"},
-        {"id": 7,"name": "Math 𝜋rate","desc": "You got a chance to show your css skills",
-        "rules": "Copying is not allowed here"},
+        
         {"id": 8,"name": "Apprentissage","desc": "You got a chance to show your css skills",
         "rules": "Copying is not allowed here"},
         {"id": 9,"name": "App Replica","desc": "You got a chance to show your css skills",
@@ -22,6 +21,8 @@ allevents = {
         {"id": 4,"name": "Mothra","desc": "You got a chance to show your css skills",
         "rules": "Copying is not allowed here"},
         {"id": 6,"name": "Conundrum","desc": "You got a chance to show your css skills",
+        "rules": "Copying is not allowed here"},
+        {"id": 7,"name": "Math 𝜋rate","desc": "You got a chance to show your css skills",
         "rules": "Copying is not allowed here"},
         {"id": 1 ,"name": "Scrim 1","desc": "This is a coding based event",
          "rules": "Solution should not be made public during the events"},
